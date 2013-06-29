b4594e80bcfefcebbf5bbae4a1d609be99d838c8
Kyle Kingsbury <kingsbury@factual.com>
2013-06-28 17:23:48 -0700

    0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject less-awful-ssl "0.1.1-SNAPSHOT"
+(defproject less-awful-ssl "0.1.1"
   :description "Get an SSLContext without wanting to rip your hair out."
   :url "http://github.com/aphyr/less-awful-ssl"
   :license {:name "Eclipse Public License"
