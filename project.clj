242ef9449a58c9b18887aee91aa37cf28dd876ac
Aphyr <aphyr@aphyr.com>
2015-01-22 00:00:16 -0800

    1.0.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject less-awful-ssl "0.1.2-SNAPSHOT"
+(defproject less-awful-ssl "1.0.0"
   :description "Get an SSLContext without wanting to rip your hair out."
   :url "http://github.com/aphyr/less-awful-ssl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :profiles {:dev {dependencies [[org.clojure/clojure "1.6.0"]]}})
