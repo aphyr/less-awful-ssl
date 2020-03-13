67fec87a3b29e0a83ecc34b469071c0ee7d38a3c
Aphyr <aphyr@aphyr.com>
2020-03-13 14:33:57 -0400

    1.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject less-awful-ssl "1.0.6-SNAPSHOT"
+(defproject less-awful-ssl "1.0.6"
   :description "Get an SSLContext without wanting to rip your hair out."
   :url "http://github.com/aphyr/less-awful-ssl"
   :license {:name "Eclipse Public License"
