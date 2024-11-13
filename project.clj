686c9379dcecb008fc14def1289ff9f816df94e3
Kyle Kingsbury <aphyr@aphyr.com>
2024-11-13 11:08:02 -0600

    Version 1.0.7

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject less-awful-ssl "1.0.7-SNAPSHOT"
+(defproject less-awful-ssl "1.0.7"
   :description "Get an SSLContext without wanting to rip your hair out."
   :url "http://github.com/aphyr/less-awful-ssl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.0"]]}})
